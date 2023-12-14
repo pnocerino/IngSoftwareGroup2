297037de786cc265de7662083f763163a315836b
m-izzo72 <152320936+m-izzo72@users.noreply.github.com>
2023-12-14 16:28:15 +0100

    Add files via upload

--- test/scientificcalculator/complexnumber/variables/VariablesTest.java
+++ test/scientificcalculator/complexnumber/variables/VariablesTest.java
@@ -1,112 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package scientificcalculator.complexnumber.variables;
 
-import javafx.collections.ObservableMap;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static scientificcalculator.ScientificCalculator.currentVariable;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
-/**
- *
- * @author utente
- */
+
 public class VariablesTest {
+    
+    Variables vars;
+    Variable variable;
     
     public VariablesTest() {
     }
     
-    @BeforeAll
+    @BeforeClass
     public static void setUpClass() {
     }
     
-    @AfterAll
+    @AfterClass
     public static void tearDownClass() {
     }
     
-    @BeforeEach
+    @Before
     public void setUp() {
+        vars = new Variables();
     }
     
-    @AfterEach
+    @After
     public void tearDown() {
     }
-
-    /**
-     * Test of getMap method, of class Variables.
-     */
+    
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+    
     @Test
-    public void testGetMap() {
-        System.out.println("getMap");
-        Variables instance = new Variables();
-        ObservableMap<Character, Variable> expResult = null;
-        ObservableMap<Character, Variable> result = instance.getMap();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCurrentVariable method, of class Variables.
-     */
-    @Test
-    public void testGetCurrentVariable() {
-        System.out.println("getCurrentVariable");
-        Variables instance = new Variables();
-        Variable expResult = null;
-        Variable result = instance.getCurrentVariable();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of add method, of class Variables.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        Variable var = null;
-        Variables instance = new Variables();
-        instance.add(var);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class Variables.
-     */
-    @Test
-    public void testRemove() {
-        System.out.println("remove");
-        char name = ' ';
-        Variables instance = new Variables();
-        Variable expResult = null;
-        Variable result = instance.remove(name);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of search method, of class Variables.
-     */
-    @Test
-    public void testSearch() {
-        System.out.println("search");
-        char name = ' ';
-        Variables instance = new Variables();
-        Variable expResult = null;
-        Variable result = instance.search(name);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testAdd1() {
+        currentVariable = 'f';
+        variable = new Variable(currentVariable, "4+33.2i");
+        vars.add(variable);
+        assertEquals(variable, vars.search(currentVariable));
     }
     
+    @Test
+    public void testAdd2() {
+        currentVariable = 'l';
+        variable = new Variable(currentVariable, "4+33.2i");
+        vars.add(variable);
+        variable = new Variable(currentVariable, "22.2-5i");
+        vars.add(variable);
+        assertEquals(variable, vars.search(currentVariable));
+    }
+    
+    @Test
+    public void testSearch1() {
+        currentVariable = 'z'; variable = new Variable(currentVariable, "6.5i");
+        vars.add(new Variable('e', "-2")); vars.add(variable); vars.add(new Variable('j', "-4i"));
+        assertEquals(variable, vars.search(currentVariable));
+    }
+    
+    @Test
+    public void testSearch2() {
+        currentVariable = 'x'; 
+        assertNull(vars.search(currentVariable));
+    }
+
 }
