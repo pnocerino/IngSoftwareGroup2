d8b3a8a06b106134b2688177837ab4f2a12cea71
CarmineVardy <150947253+CarmineVardy@users.noreply.github.com>
2023-12-14 17:28:01 +0100

    Add files via upload

--- test/scientificcalculator/complexnumber/CommandTest.java
+++ test/scientificcalculator/complexnumber/CommandTest.java
@@ -5,38 +5,331 @@
  */
 package scientificcalculator.complexnumber;
 
+import exceptions.SyntaxErrorException;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import scientificcalculator.complexnumber.variables.Variables;
+import static org.junit.Assert.*;
 
 /**
  *
- * @author utente
+ * @author sherr
  */
 public class CommandTest {
     
     public CommandTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testGetCommand1() {
+        Command command = new Command("3+4i");
+        String out = command.getCommand();
+        assertEquals("3+4i",out);
+    }
+    
+    @Test
+    public void testGetCommand2() {
+        Command command = new Command("+");
+        String out = command.getCommand();
+        assertEquals("+",out);
+    }
+    
+    @Test
+    public void testGetCommand3() {
+        Command command = new Command("clear");
+        String out = command.getCommand();
+        assertEquals("clear",out);
+    }
+    
+    @Test
+    public void testGetCommand4() {
+        Command command = new Command(">x");
+        String out = command.getCommand();
+        assertEquals(">x",out);
+    }
+     
+    @Test
+    public void testIsOperand1() {
+        Command command = new Command("-2+i");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand2() {
+        Command command = new Command("5-i8");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand3() {
+        Command command = new Command("i-8");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand4() {
+        Command command = new Command("-i7+5");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand5() {
+        Command command = new Command("12");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand6() {
+        Command command = new Command("-7i");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand7() {
+        Command command = new Command("i2");
+        assertTrue(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand8() {
+        Command command = new Command("3+3");
+        assertFalse(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand9() {
+        Command command = new Command("i++");
+        assertFalse(command.isOperand());
+    }
+    
+    @Test
+    public void testIsOperand10() {
+        Command command = new Command("7i7");
+        assertFalse(command.isOperand());
+    }
+    
+    @Test
+    public void testIsMathOperator1() {
+        Command command = new Command("+");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator2() {
+        Command command = new Command("-");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator3() {
+        Command command = new Command("*");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator4() {
+        Command command = new Command("/");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator5() {
+        Command command = new Command("sqrt");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator6() {
+        Command command = new Command("+-");
+        assertTrue(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator7() {
+        Command command = new Command("--");
+        assertFalse(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator8() {
+        Command command = new Command("++");
+        assertFalse(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator9() {
+        Command command = new Command("Sqrt");
+        assertFalse(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsMathOperator10() {
+        Command command = new Command("--");
+        assertFalse(command.isMathOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator1() {
+        Command command = new Command("clear");
+        assertTrue(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator2() {
+        Command command = new Command("drop");
+        assertTrue(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator3() {
+        Command command = new Command("dup");
+        assertTrue(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator4() {
+        Command command = new Command("swap");
+        assertTrue(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator5() {
+        Command command = new Command("over");
+        assertTrue(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIstackOperator6() {
+        Command command = new Command("CLEAR");
+        assertFalse(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator7() {
+        Command command = new Command("DRop");
+        assertFalse(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsStackOperator8() {
+        Command command = new Command("SWap");
+        assertFalse(command.isStackOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator1() {
+        Command command = new Command("+a");
+        assertTrue(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator2() {
+        Command command = new Command("-b");
+        assertTrue(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator3() {
+        Command command = new Command(">c");
+        assertTrue(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator4() {
+        Command command = new Command("<d");
+        assertTrue(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator5() {
+        Command command = new Command("a");
+        assertFalse(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator6() {
+        Command command = new Command("+A");
+        assertFalse(command.isVariableOperator());
+    }
+    
+    @Test
+    public void testIsVariableOperator7() {
+        Command command = new Command("><");
+        assertFalse(command.isVariableOperator());
+    }
+
+    @Test
+    public void testExecuteCommand1() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("CLEAR");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+    
+    @Test
+    public void testExecuteCommand2() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("2+");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+
+    @Test
+    public void testExecuteCommand3() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command(">aa");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+    
+    @Test
+    public void testExecuteCommand4() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("2+4.5");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+    
+    @Test
+    public void testExecuteCommand5() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("-+");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+    
+    @Test
+    public void testExecuteCommand6() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("<A");
+        command.executeCommand(new Stack(24), new Variables());
+    }
+    
+    @Test
+    public void testExecuteCommand7() throws SyntaxErrorException {
+        exceptionRule.expect(SyntaxErrorException.class);
+        Command command = new Command("Drop");
+        command.executeCommand(new Stack(24), new Variables());
+    }
     
 }
--- test/scientificcalculator/complexnumber/StackTest.java
+++ test/scientificcalculator/complexnumber/StackTest.java
@@ -1,138 +1,212 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scientificcalculator.complexnumber;
 
-import javafx.collections.ObservableList;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import exceptions.SyntaxErrorException;
+import exceptions.SystemErrorException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
-/**
- *
- * @author utente
- */
 public class StackTest {
-    
+   
+    Stack stack = new Stack(24);
+   
     public StackTest() {
     }
-    
-    @BeforeAll
+   
+    @BeforeClass
     public static void setUpClass() {
     }
-    
-    @AfterAll
+   
+    @AfterClass
     public static void tearDownClass() {
     }
-    
-    @BeforeEach
+   
+    @Before
     public void setUp() {
+              
     }
-    
-    @AfterEach
+   
+    @After
     public void tearDown() {
     }
 
-    /**
-     * Test of getStack method, of class Stack.
-     */
-    @Test
-    public void testGetStack() {
-        System.out.println("getStack");
-        Stack instance = null;
-        ObservableList expResult = null;
-        ObservableList result = instance.getStack();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
-    /**
-     * Test of push method, of class Stack.
-     */
     @Test
-    public void testPush() throws Exception {
-        System.out.println("push");
-        ComplexNumber n = null;
-        Stack instance = null;
-        instance.push(n);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPush1() throws SystemErrorException, SyntaxErrorException {
+        ComplexNumber num = new ComplexNumber ("5i");
+        stack.push(num);
+        assertEquals("5i", stack.peek().toString());
     }
-
-    /**
-     * Test of pop method, of class Stack.
-     */
+   
     @Test
-    public void testPop() throws Exception {
-        System.out.println("pop");
-        Stack instance = null;
-        ComplexNumber expResult = null;
-        ComplexNumber result = instance.pop();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPush2() throws SystemErrorException, SyntaxErrorException {
+        ComplexNumber num1 = new ComplexNumber ("45i");
+        stack.push(num1);
+        ComplexNumber num2 = new ComplexNumber ("77i");
+        stack.push(num2);
+        ComplexNumber num3 = new ComplexNumber ("99i");
+        stack.push(num3);
+        ComplexNumber num4 = new ComplexNumber ("8+11i");
+        stack.push(num4);
+        ComplexNumber num5 = new ComplexNumber ("7+11i");
+        stack.push(num5);
+               
+        ComplexNumber test = new ComplexNumber("6-43i");
+        stack.push(test);
+        assertEquals("6 - 43i", stack.peek().toString());
     }
-
-    /**
-     * Test of peek method, of class Stack.
-     */
+   
+    @Test(expected = SystemErrorException.class)
+    public void testPush3() throws SystemErrorException, SyntaxErrorException {
+      
+        ComplexNumber num1 = new ComplexNumber ("45i");
+        stack.push(num1);
+        ComplexNumber num2 = new ComplexNumber ("7.7i");
+        stack.push(num2);
+        ComplexNumber num3 = new ComplexNumber ("99");
+        stack.push(num3);
+        ComplexNumber num4 = new ComplexNumber ("8+11i");
+        stack.push(num4);
+        ComplexNumber num5 = new ComplexNumber ("7+11i");
+        stack.push(num5);
+        ComplexNumber num6 = new ComplexNumber ("45.3i");
+        stack.push(num6);
+        ComplexNumber num7 = new ComplexNumber ("77i");
+        stack.push(num7);
+        ComplexNumber num8 = new ComplexNumber ("99");
+        stack.push(num8);
+        ComplexNumber num9 = new ComplexNumber ("8+11i");
+        stack.push(num9);
+        ComplexNumber num10 = new ComplexNumber ("7+11i");
+        stack.push(num10);
+        ComplexNumber num11 = new ComplexNumber ("4i");
+        stack.push(num11);
+        ComplexNumber num12 = new ComplexNumber ("7i");
+        stack.push(num12);
+        ComplexNumber num13 = new ComplexNumber ("79");
+        stack.push(num13);
+        ComplexNumber num14 = new ComplexNumber ("1.8+11i");
+        stack.push(num14);
+        ComplexNumber num15 = new ComplexNumber ("7+11i");
+        stack.push(num15);
+        ComplexNumber num16 = new ComplexNumber ("45i");
+        stack.push(num16);
+        ComplexNumber num17 = new ComplexNumber ("77i");
+        stack.push(num17);
+        ComplexNumber num18 = new ComplexNumber ("96.2");
+        stack.push(num18);
+        ComplexNumber num19 = new ComplexNumber ("8+11i");
+        stack.push(num19);
+        ComplexNumber num20 = new ComplexNumber ("7+11i");
+        stack.push(num20);
+        ComplexNumber num21 = new ComplexNumber ("45i");
+        stack.push(num21);
+        ComplexNumber num22 = new ComplexNumber ("77i");
+        stack.push(num22);
+        ComplexNumber num23 = new ComplexNumber ("99");
+        stack.push(num23);
+        ComplexNumber num24 = new ComplexNumber ("8+11i");
+        stack.push(num24);       
+      
+        ComplexNumber num = new ComplexNumber("7");
+        stack.push(num);
+        assertEquals("7", stack.peek().toString());
+    }
+       
     @Test
-    public void testPeek() throws Exception {
-        System.out.println("peek");
-        Stack instance = null;
-        ComplexNumber expResult = null;
-        ComplexNumber result = instance.peek();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPop1() throws SystemErrorException, SyntaxErrorException {
+       
+        ComplexNumber num1 = new ComplexNumber ("45i");
+        stack.push(num1);
+        ComplexNumber num2 = new ComplexNumber ("77i");
+        stack.push(num2);
+        ComplexNumber num3 = new ComplexNumber ("99");
+        stack.push(num3);
+        ComplexNumber num4 = new ComplexNumber ("8+11i");
+        stack.push(num4);
+        ComplexNumber num5 = new ComplexNumber ("7+11i");
+        stack.push(num5);
+       
+        ComplexNumber top = new ComplexNumber("0");
+        top = stack.peek();
+        ComplexNumber num = new ComplexNumber("0");
+        num=stack.pop();
+        assertEquals(top, num);
     }
-
-    /**
-     * Test of getSize method, of class Stack.
-     */
+   
+    @Test(expected=SyntaxErrorException.class)
+    public void testPop2() throws SystemErrorException, SyntaxErrorException {
+       
+        stack.stack.clear();
+       
+        ComplexNumber num = new ComplexNumber("0");
+        num=stack.pop();
+    }
+   
     @Test
-    public void testGetSize() {
-        System.out.println("getSize");
-        Stack instance = null;
-        int expResult = 0;
-        int result = instance.getSize();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPop3() throws SystemErrorException, SyntaxErrorException {
+       
+        ComplexNumber num1 = new ComplexNumber ("45i");
+        stack.push(num1);
+        ComplexNumber num2 = new ComplexNumber ("7.7i");
+        stack.push(num2);
+        ComplexNumber num3 = new ComplexNumber ("99");
+        stack.push(num3);
+        ComplexNumber num4 = new ComplexNumber ("8+11i");
+        stack.push(num4);
+        ComplexNumber num5 = new ComplexNumber ("7+11i");
+        stack.push(num5);
+        ComplexNumber num6 = new ComplexNumber ("45.3i");
+        stack.push(num6);
+        ComplexNumber num7 = new ComplexNumber ("77i");
+        stack.push(num7);
+        ComplexNumber num8 = new ComplexNumber ("99");
+        stack.push(num8);
+        ComplexNumber num9 = new ComplexNumber ("8+11i");
+        stack.push(num9);
+        ComplexNumber num10 = new ComplexNumber ("7+11i");
+        stack.push(num10);
+        ComplexNumber num11 = new ComplexNumber ("4i");
+        stack.push(num11);
+        ComplexNumber num12 = new ComplexNumber ("7i");
+        stack.push(num12);
+        ComplexNumber num13 = new ComplexNumber ("79");
+        stack.push(num13);
+        ComplexNumber num14 = new ComplexNumber ("1.8+11i");
+        stack.push(num14);
+        ComplexNumber num15 = new ComplexNumber ("7+11i");
+        stack.push(num15);
+        ComplexNumber num16 = new ComplexNumber ("45i");
+        stack.push(num16);
+        ComplexNumber num17 = new ComplexNumber ("77i");
+        stack.push(num17);
+        ComplexNumber num18 = new ComplexNumber ("96.2");
+        stack.push(num18);
+        ComplexNumber num19 = new ComplexNumber ("8+11i");
+        stack.push(num19);
+        ComplexNumber num20 = new ComplexNumber ("7+11i");
+        stack.push(num20);
+        ComplexNumber num21 = new ComplexNumber ("45i");
+        stack.push(num21);
+        ComplexNumber num22 = new ComplexNumber ("77i");
+        stack.push(num22);
+        ComplexNumber num23 = new ComplexNumber ("99");
+        stack.push(num23);
+        ComplexNumber num24 = new ComplexNumber ("8+11i");
+        stack.push(num24);
+       
+        ComplexNumber top = new ComplexNumber("0");
+        top = stack.peek();
+        ComplexNumber num = new ComplexNumber("0");
+        num=stack.pop();
+        assertEquals(top, num);
     }
-
-    /**
-     * Test of isEmpty method, of class Stack.
-     */
-    @Test
-    public void testIsEmpty() {
-        System.out.println("isEmpty");
-        Stack instance = null;
-        boolean expResult = false;
-        boolean result = instance.isEmpty();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isFull method, of class Stack.
-     */
-    @Test
-    public void testIsFull() {
-        System.out.println("isFull");
-        Stack instance = null;
-        boolean expResult = false;
-        boolean result = instance.isFull();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+   
 }
